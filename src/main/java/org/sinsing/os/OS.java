2dda4e9a4fb5106a484fa4c653abc1ea41599208
soreana <esterlinkof@gmail.com>
2016-07-13 01:55:36 +0430

    create simple OS that start terminal and kill that after 10 second.

---
+++ src/main/java/org/sinsing/os/OS.java
@@ -0,0 +1,19 @@
+package org.sinsing.os;
+
+import org.sinsing.os.applications.Terminal;
+
+/**
+ * Created by sosin-PC on 7/13/2016.
+ */
+public class OS {
+    public static void start(){
+        try {
+            Thread terminator = new Thread(new Terminal(System.in));
+            terminator.start();
+            Thread.sleep(10000);
+            terminator.interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
