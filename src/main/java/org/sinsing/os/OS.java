dac0899d41dd19e3657e13f352b32c82b5c87771
soreana <esterlinkof@gmail.com>
2016-08-10 23:38:08 +0430

    add stop functionality to OS.

--- src/main/java/org/sinsing/os/OS.java
+++ src/main/java/org/sinsing/os/OS.java
@@ -2,18 +2,22 @@ package org.sinsing.os;
 
 import org.sinsing.os.applications.terminal.Terminal;
 
+import java.util.ArrayList;
+
 /**
  * Created by sosin-PC on 7/13/2016.
  */
 public class OS {
-    public static void start(){
-        try {
-            Thread terminator = new Thread(new Terminal(System.in));
-            terminator.start();
-            Thread.sleep(10000);
-            terminator.interrupt();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    private static ArrayList<Thread> threads =
+            new ArrayList<>();
+
+    public static void start() {
+        Thread temp = new Thread(new Terminal(System.in));
+        threads.add(temp);
+        temp.start();
+    }
+
+    public static void stop() {
+        threads.forEach(Thread::interrupt);
     }
 }
